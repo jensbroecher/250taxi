1749d05974a35e743d34921b4d55ead23f0e0e3c
jensbroecher <jensbroecher@gmail.com>
2015-10-20 00:53:25 +0200

    new menu

--- pushmenu/mainmenu.js
+++ pushmenu/mainmenu.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
     
+$( "#mainmenu_services" ).click(function() {
+    document.location.href = 'services.html';
+});
+    
 $( "#mainmenu_help" ).click(function() {
-
+    
 $( "#pagestarget" ).load( "http://250taxi.com/appcontent/help.php", function() {
   $( "#pages" ).fadeIn( "slow", function() {
     // Animation complete
