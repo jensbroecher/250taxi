f1c02cee6bb361a6ddf679591c800d7e2b64c72e
jensbroecher <jensbroecher@gmail.com>
2016-05-24 15:35:02 +0200

    x

--- oauth/oauth.js
+++ oauth/oauth.js
@@ -193,7 +193,7 @@ var email_from_google = localStorage.getItem("email_from_google");
 $.get("https://250taxi.com/db/username_from_email.php?username=" + email_from_google + "", function(username) {
     
 if (username == "") {
-    alert("Account not found. Please register first.");zuuuz7
+    alert("Account not found.<br>Please register first.");
     start_reg();
     return;
 }
