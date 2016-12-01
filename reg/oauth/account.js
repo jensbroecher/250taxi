9110a5c1a2bdf74bcaaa6c4392867e3d95336a7b
jensbroecher <jensbroecher@gmail.com>
2016-12-01 20:16:14 +0100

    x

--- reg/oauth/account.js
+++ reg/oauth/account.js
@@ -64,10 +64,17 @@ function social_login_go() {
     var device_uuid = localStorage.getItem("device_uuid");
     var device_battery = localStorage.getItem("device_battery");
 
-    $.get("https://enunua.com/ubdream/db/check-email.php?task=userid&email=" + sign_in_email + "", function (userid) {
+    $.get("https://250taxi.com/db/check_user.php?task=getuserid&email=" + sign_in_email + "", function (userid) {
 
         localStorage.setItem("userid", userid);
         localStorage.setItem("rememberuser", "Yes");
+        
+        localStorage.setItem("rememberuser", "Yes");
+
+        localStorage.removeItem("hotelcorporate");
+
+        localStorage.setItem("logupdate", "" + userid + "*0*login*User" + userid + " logged in trough social.");
+        logupdate_v2();
 
         $.get("https://enunua.com/ubdream/db/set_randomclientid.php?&userid=" + userid + "&randomclientid=" + randomclientid + "&device_model=" + device_model + "&device_platform=" + device_platform + "&device_version=" + device_version + "&device_uuid=" + device_uuid + "&device_battery=" + device_battery + "", function (data) {
 
@@ -93,32 +100,27 @@ function social_register_go() {
         var sign_in_name = localStorage.getItem("google_name");
     }
 
-    swal("", "" + sign_in_name + ", " + sign_in_email + " is not yet registered with Afritaxi. \n\nPlease tell as your phone number and pick a password.");
+    swal("", "" + sign_in_name + ", you're not yet registered with Afritaxi. \n\nPlease tell as your phone number and pick a password.");
 
     $("#reg_start").load("reg/mainmenu.html", function () {
 
-        $.get('reg/social_login.html', function (data) {
-            $('#reg_form').prepend(data);
+        var name_from_social_split = sign_in_name.split(" ");
 
-            social_login_btn();
+        document.getElementById("first_name").value = name_from_social_split[0];
+        document.getElementById("last_name").value = name_from_social_split[1];
 
-        });
+        document.getElementById("email").value = sign_in_email;
+
+        Materialize.updateTextFields();
 
         document.getElementById("reg_start").style.height = "100%";
-        if (document.getElementById("close_reg_start_arrow"))
+        if (document.getElementById("close_reg_start_arrow")) {
             document.getElementById("close_reg_start_arrow").style.marginLeft = "10px";
+        }
+
     });
 
     location.href = "#registration";
     localStorage.setItem("reg_start_open", "Yes");
-
-    var name_from_social_split = sign_in_name.split(" ");
-
-    document.getElementById("first_name").value = name_from_social_split[0];
-    document.getElementById("last_name").value = name_from_social_split[1];
-
-    document.getElementById("email").value = sign_in_email;
-
-    Materialize.updateTextFields();
 
 }
